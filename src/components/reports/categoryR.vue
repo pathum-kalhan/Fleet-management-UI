7af797e4137fc7fe3b12b5d8a94a5e9813bf9e5d
pathumkalhan <pathumsimpson@gmail.com>
2020-01-15 22:17:30 +0530

    category report

--- src/components/reports/categoryR.vue
+++ src/components/reports/categoryR.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout row wrap justify-center>
-    <v-flex xs12 sm12 md6>
+    <v-flex xs12 sm12 md8>
       <v-card>
         <v-card-title>
           <h1>Category Report</h1>
@@ -24,10 +24,15 @@
                 <v-radio label="All" value="All"></v-radio>
               </v-radio-group>
             </v-flex>
-            <v-flex xs12 sm12 md12>
-              <v-subheader>Created from</v-subheader>
-              <v-date-picker :landscape="true" v-model="dateFrom" :max="maxDate"></v-date-picker>
+            <v-flex xs12 sm12 md6>
+              <v-subheader>From</v-subheader>
+              <v-date-picker  v-model="dateFrom" :max="maxDate"></v-date-picker>
             </v-flex>
+            <v-flex xs12 sm12 md6>
+              <v-subheader>To</v-subheader>
+              <v-date-picker  v-model="dateTo" :max="maxDate"></v-date-picker>
+            </v-flex>
+            <p style="color:red">From date should be greather than To date.</p>
           </v-layout>
         </v-card-text>
         <v-card-actions>
@@ -49,78 +54,87 @@
 </template>
 
 <script>
-import JsonExcel from "vue-json-excel";
-import { required } from "vuelidate/lib/validators";
+import JsonExcel from 'vue-json-excel';
+import { required } from 'vuelidate/lib/validators';
 
+const date_greather_than = (value, vm) => {
+  const from = new Date(vm.dateFrom);
+  const to = new Date(value);
+  return from <= to;
+};
 export default {
   mounted() {
-    this.maxDate = this.$moment().format("YYYY-MM-DD");
+    this.maxDate = this.$moment().format('YYYY-MM-DD');
   },
   validations: {
     dateFrom: { required },
+    dateTo: { required, date_greather_than },
+
     orderBy: { required },
-    status: { required }
+    status: { required },
   },
   components: {
-    JsonExcel
+    JsonExcel,
   },
   data() {
     return {
-      maxDate:"",
+      maxDate: '',
       json_fields: {
-        Id: "id",
-        Name: "name",
-        Description: "description",
-        Status: "status",
-        createdAt: "createdAt",
-        UpdatedAt: "updatedAt"
+        Id: 'id',
+        Name: 'name',
+        Description: 'description',
+        Status: 'status',
+        createdAt: 'createdAt',
+        UpdatedAt: 'updatedAt',
       },
       orderByTypes: [
-        { text: "Name Ascending", value: "name" },
-        { text: "Name Descending", value: "name DESC" },
-        { text: "Status Ascending", value: "status" },
-        { text: "Status Descending", value: "status DESC" },
-        { text: "Created Date Ascending", value: "createdAt" },
-        { text: "Created Date Descending", value: "createdAt DESC" }
+        { text: 'Name Ascending', value: 'name' },
+        { text: 'Name Descending', value: 'name DESC' },
+        { text: 'Status Ascending', value: 'status' },
+        { text: 'Status Descending', value: 'status DESC' },
+        { text: 'Created Date Ascending', value: 'createdAt' },
+        { text: 'Created Date Descending', value: 'createdAt DESC' },
       ],
-      orderBy: "",
-      status: "",
-      dateFrom: "",
-      alertType: "error",
-      alert: "Error while loading the data from api...",
-      hasAlert: false
+      orderBy: '',
+      status: '',
+      dateFrom: '',
+      alertType: 'error',
+      alert: 'Error while loading the data from api...',
+      hasAlert: false,
+      dateTo: '',
     };
   },
   methods: {
     async POST() {
       try {
         const formData = {
-          dateFrom: this.dateFrom,
+          from: this.dateFrom,
+          to: this.dateTo,
           orderBy: this.orderBy,
-          status: this.status
+          status: this.status,
         };
         if (this.$v.$invalid) {
-          this.alertType = "error";
-          this.alert = "Please fill all the required fields.";
+          this.alertType = 'error';
+          this.alert = 'Please fill all the required fields.';
           this.hasAlert = true;
           return;
         }
-       
-        const data = await this.$http.post("/reports/category", formData);
+
+        const data = await this.$http.post('/reports/category', formData);
         if (data.data.length === 0) {
-          this.alertType = "error";
-          this.alert = "No data available!";
+          this.alertType = 'error';
+          this.alert = 'No data available!';
           this.hasAlert = true;
           return;
         }
-       
+
         return data.data;
       } catch (error) {
-        this.alertType = "error";
-        this.alert = "Some thing went wrong!";
+        this.alertType = 'error';
+        this.alert = 'Some thing went wrong!';
         this.hasAlert = true;
       }
-    }
-  }
+    },
+  },
 };
 </script>
